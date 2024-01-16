34e758a8466c6f8769f139b537c7e30397860ac3
F-Dupraz <duprazfabricio@gmail.com>
2024-01-16 17:56:51 -0300

    Improving the ReadCSV so now it can read ell the csvs in the folder for my next idea

--- csvReader/csvReader.go
+++ csvReader/csvReader.go
@@ -15,8 +15,34 @@ func NewReadCsv() *ReadCsv {
 	return &ReadCsv{}
 }
 
+func (rc *ReadCsv) GetCSVs() []string {
+	var myCSVs []string
+
+	myDir, err := os.Getwd()
+	if err != nil {
+		fmt.Errorf("%v", err)
+		return nil
+	}
+
+	myDirDocuments, err := os.ReadDir(myDir)
+	if err != nil {
+		fmt.Errorf("%v", err)
+		return nil
+	}
+
+	for _, doc := range myDirDocuments {
+		docName := doc.Name()
+		extension := len(docName) - 3
+		if docName[extension:] == "csv" {
+			myCSVs = append(myCSVs, docName)
+		}
+	}
+
+	return myCSVs
+}
+
 // GetTargetValues gets target values from a CSV file
-func (rc *ReadCsv) GetTargetValues(filepath string, hasHeader bool, columnIndex uint) ([]float64, error) {
+func (rc *ReadCsv) GetTargetValues(filepath string, hasHeader bool) ([]float64, error) {
 	// Open the CSV file
 	file, err := os.Open(filepath)
 	if err != nil {
@@ -44,82 +70,83 @@ func (rc *ReadCsv) GetTargetValues(filepath string, hasHeader bool, columnIndex 
 			break
 		}
 
-		// Ensure the record has enough fields
-		if len(record) <= int(columnIndex) {
-			return nil, fmt.Errorf("Error: The line does not have enough fields")
-		}
+		// Ensure the record has at least one field
+		if len(record) > 0 {
+			// Convert the last field to a double and add it to the vector
+			value, err := strconv.ParseFloat(record[len(record)-1], 64)
+			if err != nil {
+				return nil, fmt.Errorf("Error converting to double: %v", err)
+			}
 
-		// Convert the field to a double and add it to the vector
-		value, err := strconv.ParseFloat(record[columnIndex], 64)
-		if err != nil {
-			return nil, fmt.Errorf("Error converting to double: %v", err)
+			columnData = append(columnData, value)
+		} else {
+			return nil, fmt.Errorf("Error: The line does not have any fields")
 		}
-
-		columnData = append(columnData, value)
 	}
 
 	return columnData, nil
 }
 
-// GetIndependentVariables gets independent variables from a CSV file
-func (rc *ReadCsv) GetIndependentVariables(filepath string, hasHeader bool, columnIndices []uint) ([][]float64, error) {
-	// Open the CSV file
+// GetIndependentVariables obtiene variables independientes desde un archivo CSV
+func (rc *ReadCsv) GetIndependentVariables(filepath string, hasHeader bool) ([][]float64, error) {
+	// Abrir el archivo CSV
 	file, err := os.Open(filepath)
 	if err != nil {
-		return nil, fmt.Errorf("Unable to open the file: %v", err)
+		return nil, fmt.Errorf("No se puede abrir el archivo: %v", err)
 	}
 	defer file.Close()
 
-	// Create a CSV reader
+	// Crear un lector de CSV
 	reader := csv.NewReader(file)
 
-	// Read the header if it exists
+	// Leer el encabezado si existe
 	if hasHeader {
 		_, err := reader.Read()
 		if err != nil {
-			return nil, fmt.Errorf("Error reading header: %v", err)
+			return nil, fmt.Errorf("Error al leer el encabezado: %v", err)
 		}
 	}
 
-	// Initialize the 2D slice to store data from multiple columns
-	columnsData := make([][]float64, len(columnIndices))
+	// Inicializar la matriz 2D para almacenar datos de múltiples columnas
+	var columnsData [][]float64
 
-	// Read data from the CSV file
+	// Leer datos del archivo CSV
 	for {
 		record, err := reader.Read()
 		if err != nil {
 			break
 		}
 
-		// Initialize currentIndex to keep track of the column index
-		currentIndex := uint(0)
-
-		// Parse each field in the line
-		for _, field := range record {
-			// Check if the current column index is within the specified column indices
-			if currentIndex < uint(len(columnIndices)) {
-				// Check if the current index is in the specified column indices
-				if contains(columnIndices, currentIndex) {
-					// Convert the field to a double and add it to the corresponding vector
-					value, err := strconv.ParseFloat(field, 64)
-					if err != nil {
-						return nil, fmt.Errorf("Error converting to double: %v - Field: %v", err, field)
-					}
-
-					columnsData[currentIndex] = append(columnsData[currentIndex], value)
+		// Verificar que la fila tiene al menos una columna
+		if len(record) > 0 {
+			// Convertir los campos a números flotantes y agregarlos a la matriz
+			var rowData []float64
+			for _, field := range record {
+				value, err := strconv.ParseFloat(field, 64)
+				if err != nil {
+					return nil, fmt.Errorf("Error al convertir a número flotante: %v - Campo: %v", err, field)
 				}
-
-				// Increment currentIndex after processing each field
-				currentIndex++
-			} else {
-				// Ignore additional fields if there are more than specified in columnIndices
-				break
+				rowData = append(rowData, value)
 			}
+
+			columnsData = append(columnsData, rowData)
+		} else {
+			return nil, fmt.Errorf("Error: La fila no tiene campos")
 		}
 	}
 
-	return columnsData, nil
+	// Transponer los datos directamente en columnsData
+	transposedData := make([][]float64, len(columnsData[0]))
+	for i := range transposedData {
+		transposedData[i] = make([]float64, len(columnsData))
+		for j := range columnsData {
+			transposedData[i][j] = columnsData[j][i]
+		}
+	}
+
+	return transposedData, nil
 }
+
 
 // contains checks if a value is present in a slice of uint
 func contains(slice []uint, value uint) bool {
--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"os"
-	"strings"
 
 	LN "github.com/F-Dupraz/LinearRegressionApp/LinearRegression"
 	Reader "github.com/F-Dupraz/LinearRegressionApp/csvReader"
@@ -14,63 +13,47 @@ func main() {
 	linearRegression := LN.NewLinearRegression()
 	myCsvReader := Reader.NewReadCsv()
 
-	// User input for file path, target column index, number of independent variables, and their indices
-	var filepath string
-	fmt.Print("Enter the path to the file: ")
-	fmt.Scanln(&filepath)
+	myDocuments := myCsvReader.GetCSVs()
 
-	var targetIndexY uint
-	fmt.Print("Enter the column index of the dependent variable (Y): ")
-	fmt.Scan(&targetIndexY)
+	var hasHeader bool = true
 
-	var numberOfValues int
-	fmt.Print("How many independent variables do you have? ")
-	fmt.Scan(&numberOfValues)
-
-	var targetIndexX []uint
-	for i := 0; i < numberOfValues; i++ {
-		var index uint
-		fmt.Print("Enter one of the indices for the independent variable (X): ")
-		fmt.Scan(&index)
-		targetIndexX = append(targetIndexX, index)
-	}
-
-	var hasHeader string
-	fmt.Print("Does your file have a header? (y/n) ")
-	fmt.Scan(&hasHeader)
-	hasHeader = strings.ToLower(hasHeader)
-
-	// Read CSV data for independent (X) and dependent (Y) variables
-	X_values, err := myCsvReader.GetIndependentVariables(filepath, hasHeader == "y", targetIndexX)
-	if err != nil || len(X_values) == 0 {
-		fmt.Println("Error: Unable to read independent variables.")
-		os.Exit(1)
-	}
-
-	Y_values, err := myCsvReader.GetTargetValues(filepath, hasHeader == "y", targetIndexY)
-	if err != nil || len(Y_values) == 0 {
-		fmt.Println("Error: Unable to read target values.")
-		os.Exit(1)
-	}
-
-	// Error handling for different data lengths
-	if len(X_values[0]) != len(Y_values) {
-		fmt.Println("Error: Different number of data points for the variables.")
-		os.Exit(1)
-	}
-
-	// Fit the linear regression model and display results
-	if linearRegression.Fit(X_values, Y_values) == 1 {
-		fmt.Println("\nIntercept:", linearRegression.GetIntercept())
-		fmt.Print("Coefficients: ")
-		for _, coef := range linearRegression.GetSlope() {
-			fmt.Print(coef, " ")
+	for _, name := range myDocuments {
+		// Read CSV data for independent (X) and dependent (Y) variables
+		X_values, err := myCsvReader.GetIndependentVariables(name, hasHeader)
+		if err != nil || len(X_values) == 0 {
+			fmt.Println("Error: Unable to read independent variables.")
+			os.Exit(1)
 		}
-		fmt.Println("\n")
-	} else {
-		fmt.Println("Error in linear regression fit.")
+
+		Y_values, err := myCsvReader.GetTargetValues(name, hasHeader)
+		if err != nil || len(Y_values) == 0 {
+			fmt.Println("Error: Unable to read target values.")
+			os.Exit(1)
+		}
+
+		fmt.Println("This is the length of the X values: ", len(X_values))
+		fmt.Println("This is the length of the X[1] values: ", len(X_values[1]))
+		fmt.Println("This is the length of the Y values: ", len(Y_values))
+
+		// // Error handling for different data lengths
+		if len(X_values[0]) != len(Y_values) {
+			fmt.Println("Error: Different number of data points for the variables.")
+			os.Exit(1)
+		}
+
+		// Fit the linear regression model and display results
+		if linearRegression.Fit(X_values, Y_values) == 1 {
+			fmt.Println("\nIntercept:", linearRegression.GetIntercept())
+			fmt.Print("Coefficients: ")
+			for _, coef := range linearRegression.GetSlope() {
+				fmt.Print(coef, " ")
+			}
+			fmt.Println("\n")
+		} else {
+			fmt.Println("Error in linear regression fit.")
+		}
 	}
 
-	prediction := linearRegression.Predict([]float64{63.0, 23.085, 0})
-	fmt.Println("The prediction is:", prediction, "\n")
+	// prediction := linearRegression.Predict([]float64{63.0, 23.085, 0})
+	// fmt.Println("The prediction is:", prediction, "\n")
 }
